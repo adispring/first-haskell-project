d6a715381a16f749bc3047acb06b37bf65b628c6
wangzengdi <sunnyadi@163.com>
2019-02-23 20:35:05 +0800

    rollDiceIO can work

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,15 @@
 module Main where
 
-import Data.Char
-import Data.List
+import           Data.Char
+import           Data.List
 
-import Lib
-import StateMonadDemo
+import           Lib
+import           StateMonadDemo
 
 main :: IO ()
 main = do
-  rollDiceIO
+  (a, b) <- rollDiceIO
+  putStrLn ("a: " ++ show a ++ ",b: " ++ show b)
   return ()
 
 
