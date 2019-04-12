dd8faccd58be7a48db9a62d0cdbf29c9706b0018
wangzengdi <sunnyadi@163.com>
2019-04-12 08:36:46 +0800

    upgrade ghc to 8.6.3 in package dependencies & transformers in stack extra-deps

--- src/MonadTransformers.hs
+++ src/MonadTransformers.hs
@@ -1,5 +1,7 @@
 module MonadTransformers (askPassphrase) where
 
+import           Control.Monad
+-- import           Control.Monad.Trans.Maybe
 import           Data.Char
 
 getPassphrase :: IO (Maybe String)
@@ -19,3 +21,16 @@ askPassphrase = do putStrLn "Insert your new passphrase:"
                    case maybe_value of
                         Just value -> do putStrLn "Storing in Database"
                         Nothing    -> putStrLn "Passphrase invalid..."
+
+-- getPassphrase1 :: MaybeT IO String
+
+-- getPassphrase2 :: MaybeT IO String
+-- getPassphrase2 = do s <- lift getLine
+--                     guard (isValid s) -- Alternative provides guard.
+--                     return s
+
+-- askPassphrase2 :: MaybeT IO ()
+-- askPassphrase2 = do lift $ putStrLn "Insert your new passphrase:"
+--                     value <- getPassphrase2
+--                     lift $ putStrLn "Storing in database..."
+
